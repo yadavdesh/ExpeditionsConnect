88c6caf3d639a68dc7688e5b34727a12fc8db260
yadavdesh <bhardwajdesh@gmail.com>
2019-09-07 16:45:08 +0200

    section how it works has introduced

--- js/sliding.js
+++ js/sliding.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
   $("#myCarousel").on("slid.bs.carousel", function() {
     //alert("slid");
   });
+
+  $("#expeditionCarousel").carousel({
+    interval: 10000
+  });
+
+  $("#expeditionCarousel").on("slid.bs.carousel", function() {
+    //alert("slid");
+  });
 });
