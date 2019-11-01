d3472b2517cfe882117f3179015a6357dc0873bc
yadavdesh <bhardwajdesh@gmail.com>
2019-11-01 17:45:28 +0100

    thank you email to usres rollback

--- php/subscription.php
+++ php/subscription.php
@@ -2,22 +2,14 @@
 $email = $_POST['sub_email'];
 
 $to = 'info@expeditionsconnect.com';
-$to_user = $email
 $subject = 'Expeditions Connect Subscription';
-$sub_reply = 'Thank you for subscribing Expeditions Connect'
-
 $message = 'Subscription Request Received From Expeditions Connect:<br/><br/>';
 $message .= '<strong>Email : </strong>'.$email.'<br/><br/>';
-$reply = 'Thank you for subscribing our page. We will contact you once we will launch'
-$reply .= '<strong>Email : </strong>'.$email.'<br/><br/>';
-
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= 'From: <'.$email.'>' . "\r\n";
 
 mail($to,$subject,$message,$headers);
-header( "Location: https://expeditionsconnect.com/index.html#subscriptionSuccess" );
-mail($to_user,$sub_reply,$message);
 header( "Location: https://expeditionsconnect.com/index.html#subscriptionSuccess" );
 
 #echo "Thank you for subscribing our page" . " -" . "<a href='../index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
