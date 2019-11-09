6eec8c53fd03117f3a04cfa68fa7a630533b1d82
yadavdesh <bhardwajdesh@gmail.com>
2019-11-09 15:34:48 +0100

    thank you modal fix in hph file

--- php/subscription.php
+++ php/subscription.php
@@ -2,13 +2,11 @@
 $email = $_POST['sub_email'];
 $to = 'info@expeditionsconnect.com';
 $subject = 'Expeditions Connect Subscription';
-$sub_reply = 'Thank you for subscribing Expeditions Connect'
 $message = 'Subscription Request Received From Expeditions Connect:<br/><br/>';
 $message .= '<strong>Email : </strong>'.$email.'<br/><br/>';
-$reply = 'Thank you for subscribing our page. We will contact you once we will launch'
-$reply .= '<strong>Email : </strong>'.$email.'<br/><br/>';
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= 'From: <'.$email.'>' . "\r\n";
+
 mail($to,$subject,$message,$headers);
 header( "Location: https://expeditionsconnect.com/index.html#subscriptionSuccess" );
