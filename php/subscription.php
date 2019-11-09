6d7173c0cb6f905c698d97db322c914f107404ca
yadavdesh <bhardwajdesh@gmail.com>
2019-11-09 16:59:43 +0100

    thankyou page redirection fix

--- php/subscription.php
+++ php/subscription.php
@@ -10,4 +10,4 @@ $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= 'From: <'.$email.'>' . "\r\n";
 
 mail($to,$subject,$message,$headers);
-header( "Location: https://expeditionsconnect.com/index.html#subscriptionSuccess; refresh:5; url=http://www.expeditionsconnect.com" );
+header( "Location: https://expeditionsconnect.com/index.html#subscriptionSuccess" );
