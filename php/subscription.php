4da8338bac9e5c4cc4fb6fc38fcb8c5698b7fe6b
yadavdesh <bhardwajdesh@gmail.com>
2019-11-10 17:45:22 +0100

    Thank you email styling v4

--- php/subscription.php
+++ php/subscription.php
@@ -15,14 +15,13 @@ $headers1 .= 'From: <'.$to.'>' . "\r\n";
 $subject1 = 'Thank you for Subscribing Expeditions Connect';
 $replymsg = 'Hello'.$email.'<br/><br/>';
 $replymsg .= 'Thank you for subscribing \"Expeditions Connect\". We will inform you once we launch in your area. <br/><br/>';
-
-$footer = 'Best Regards.<br/><br/>';
-$footer .= 'Expeditions Connect';
+$replymsg  .= 'Best Regards.<br/>';
+$replymsg  .= 'Expeditions Connect';
 
 
 mail($to,$subject,$message,$headers);
 header( "Location: https://expeditionsconnect.com#subscriptionSuccess" );
 
 
-$replymsg = "Thank you for subscribing \"Expeditions Connect\". We will inform you once we launch in your area. ";
-mail($email, $subject1, $replymsg, $footer, $headers1);
+#$replymsg = "Thank you for subscribing \"Expeditions Connect\". We will inform you once we launch in your area. ";
+mail($email, $subject1, $replymsg, $headers1);
