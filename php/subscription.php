1b42286d04c0e96f8e84fd0912f29e09a37bcb93
yadavdesh <bhardwajdesh@gmail.com>
2019-11-10 16:55:35 +0100

    Thank you email fix

--- php/subscription.php
+++ php/subscription.php
@@ -9,9 +9,13 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= 'From: <'.$email.'>' . "\r\n";
 
+$headers1 = "MIME-Version: 1.0" . "\r\n";
+$headers1 .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers1 .= 'From: <'.$to.'>' . "\r\n";
+
 mail($to,$subject,$message,$headers);
 header( "Location: https://expeditionsconnect.com#subscriptionSuccess" );
 
 
 $replymsg = "Thank you for subscribing \"Expeditions Connect\". We will inform you once we launch in your area. ";
-mail($email, $subject, $replymsg, $headers);
+mail($email, $subject, $replymsg, $headers1);
