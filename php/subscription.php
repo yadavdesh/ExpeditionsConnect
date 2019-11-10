1888660613dcb7061ff71b6ba91a115c9b9b97ad
yadavdesh <bhardwajdesh@gmail.com>
2019-11-10 17:09:51 +0100

    Thank you email styling

--- php/subscription.php
+++ php/subscription.php
@@ -12,6 +12,15 @@ $headers .= 'From: <'.$email.'>' . "\r\n";
 $headers1 = "MIME-Version: 1.0" . "\r\n";
 $headers1 .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers1 .= 'From: <'.$to.'>' . "\r\n";
+$replymsg = 'Hello <'.$email.'>' . "\r\n";
+$replymsg .= "\r\n";
+$replymsg .= "\r\n";
+$replymsg .= "Thank you for subscribing \"Expeditions Connect\". We will inform you once we launch in your area." . "\r\n";
+$replymsg .= "\r\n";
+$replymsg .= "\r\n";
+$replymsg .= "Best Regards"."\r\n";
+$replymsg .= "Expeditions Connect";
+
 
 mail($to,$subject,$message,$headers);
 header( "Location: https://expeditionsconnect.com#subscriptionSuccess" );
