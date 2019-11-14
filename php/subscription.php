9156330e16add8299f399a7222b40328a314aa95
yadavdesh <bhardwajdesh@gmail.com>
2019-11-14 14:32:20 +0100

    thank you email design and desing fixes v3

--- php/subscription.php
+++ php/subscription.php
@@ -23,7 +23,7 @@ $replymsg = '
         <title>Welcome to Expeditions Connect</title> 
     </head> 
     <body> 
-    <div style="width: 80%; margin: auto; background-color:rgb(198, 210, 243); text-align: center;">
+    <div style="width: 80%; margin: auto; margin-top: 30px; margin-bottom: 30px; background-color:rgb(198, 210, 243); text-align: center;">
         <img src="www.expeditionsconnect/images/logo/logo.png">
         <h1 style="color: #3a63c2">Welcome to Expeditions Connect</h1>
         <h3>Thanks you for joining with us!</h3> 
